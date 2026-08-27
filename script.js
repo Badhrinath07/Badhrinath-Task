b7c1e3a6b938b07565800226d55bec7131ba5cf8
Badhrinath07 <manimaddebadrinath@gmail.com>
2026-08-27 13:33:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. Create variables for student name, age and mark and print all three
+let studentName = "Badhrinath";
+let age = 26;
+let mark = 85;
+
+console.log(studentName);
+console.log(age);
+console.log(mark);
+
+// 2. Ask user's name using promt() and display it using alert()
+let name = prompt("Enter your name");
+alert(name);
+
+// 3. Ask user's age using promt () and print it using console.log()
+let UserAge = prompt("Enter your age");
+console.log(UserAge);
+
+// 4. Ask then user a question using confirm()
+confirm("Are you sure?");
+
+// 5. Ask user's name and display it on the webpage using document.writeln()
+let UserName = prompt("Enter your name");
+document.writeln(UserName);
